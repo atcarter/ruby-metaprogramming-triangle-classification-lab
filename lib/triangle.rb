bccaa1221b11be1ebf1466f6d5eac6464a04e98d
Antonio <atcarter005@gmail.com>
2021-01-17 17:37:18 -0600

    commit tree daa27402d41350bb4da84709451635ceea4d5c50

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
-end
+  # triangle code
+
+  attr_accessor :a, :b, :c
+
+  def initialize(s1, s2, s3)
+    @a = s1
+    @b = s2
+    @c = s3
+  end
+
+  def kind
+    caseab = @a + @b
+    casebc = @b + @c
+    caseac = @a + @c
+    if @a > 0 && @b > 0 && @c > 0  && caseab > @c && caseac > @b && casebc > @a
+      if @a == @c && @b == @c
+        :equilateral
+      elsif @a == @c || @b == @c || @a == @b
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+end 
